4051fbdfed02deefe5124c6831c1a3708e54e7a2
Tylor <tylorlouis@ucla.edu>
2013-11-12 23:19:13 -0800

    Implement barcode post to server, verify reciept

--- widget/frontend/js/priceWidget.js
+++ widget/frontend/js/priceWidget.js
@@ -1,3 +1,80 @@
 $( document ).ready( function() {
+    //*******************************************
+    //* INIT CLASSES
+    //*******************************************
+	widget = new PriceRecWidget();
+
+    //*******************************************
+    //* SET INIT STATE
+    //*******************************************
 	$('div.recommendations').hide();
+
+    //*******************************************
+    //* SET EVENT LISTENERS
+    //*******************************************
+	$('#submit-barcode').bind('click', function( event ) {
+        event.preventDefault();
+        event.stopPropagation();
+		widget.SubmitBarcode();
+	});
+
 });
+
+
+var PriceRecWidget = function() {
+	var self = this;
+	var MAXBARCODELENGTH = 120; //TODO: Fix this number.
+
+	self.SubmitBarcode = function() {
+		var barcode = $( '#barcode' ).val();
+		if ( !barcode ) {
+			self.ReportError( 'Please input a barcode' );
+			return;
+		}
+		if ( barcode.length >  MAXBARCODELENGTH ) {
+			self.ReportError( 'Please input a valid barcode' );
+			return;
+		}
+
+		Request({
+			url: '/priceRec',
+			type: 'POST',
+			data: {
+				barcode: barcode
+			}
+		},
+		function( error, message ) {
+			// TODO: Change this to actual functionality..not indication of receipt
+			self.ReportError( error ? error : message );
+		});
+	};
+
+	self.ReportError = function( error ) {
+		$('.price-rec .error-feedback').html(error);
+	};
+
+	self.ShowRecommendation = function( ) {
+		// TODO
+	};
+}
+
+
+
+// API Request Helper
+Request = function( params, callback ) {
+    $.ajax({
+        url: params.url,
+        type: params.type || 'GET',
+        data: params.data || null,
+        cache: params.cache || false,
+        success: function( data ) {
+            data = typeof( params.message ) !== 'undefined' && params.message === false && typeof( data.message ) !== 'undefined' ? data : data.message;
+            if ( callback ) callback( null, data );
+        },
+        error: function( xhr ) {
+            return;
+            var jsonError = JSON.parse( xhr.responseText );
+            if ( callback ) callback( jsonError && jsonError.message ? jsonError.message : xhr.responseText, null );
+        }
+    });
+}
--- widget/server/server.js
+++ widget/server/server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 require('./global.js')
 var api = require(API_CORE);
-var app = express();
 
-// Static Files
+
+var app = express();
+app.use(express.bodyParser());
 app.use(express.static(WEB_PATH));
 
-app.get('/hello', function(request, response){
-	JsonResponse('Hi Everybody! :D', response, 200);
+app.get( '/hello', function( request, response ) {
+	api.JsonResponse('Hi Everybody! :D', response, 200);
 });
 
-app.get('/my_account', function(request, response){
+app.get( '/my_account', function( request, response ) {
 	api.RenderPage({
         template: '130_static_mock.mustache',
         response: response,
@@ -26,6 +27,10 @@ app.get('/my_account', function(request, response){
             '/js/lib/jquery-1.10.2.min.js'
         ]
     });
+});
+
+app.post( '/priceRec', function( request, response ) {
+    api.JsonResponse( 'Got the barcode, will you get this message?', response, 200 );
 });
 
 app.listen(8000);
